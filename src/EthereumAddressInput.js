9d42bd3f8e0302f066ce6ec6c531a132e397025c
Benjamin Wasty <benny.wasty@gmail.com>
2023-07-04 10:56:25 +0000

    prettier

--- src/EthereumAddressInput.js
+++ src/EthereumAddressInput.js
@@ -1,62 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { getAddress } from 'ethers';
+import React, { useState, useEffect } from "react";
+import { getAddress } from "ethers";
 
 const validateAddress = async (address, provider) => {
   try {
     // If the address ends with '.eth', it's an ENS name
-    if (address.endsWith('.eth')) {
+    if (address.endsWith(".eth")) {
       // Use ethers.js to resolve the ENS name to an address
       const resolvedAddress = await provider.resolveName(address);
       if (resolvedAddress) {
-        return {address: resolvedAddress, ensName: address};
+        return { address: resolvedAddress, ensName: address };
       } else {
-        throw new Error('ENS name could not be resolved');
+        throw new Error("ENS name could not be resolved");
       }
     } else {
       // If it's not an ENS name, normalize and validate it as an Ethereum address
-      return {address: getAddress(address)};
+      return { address: getAddress(address) };
     }
   } catch {
     return false;
   }
 };
 
-
-const processAddress = async (address, setAddressBundle, ensMapping, setEnsMapping, provider) => {
+const processAddress = async (
+  address,
+  setAddressBundle,
+  ensMapping,
+  setEnsMapping,
+  provider
+) => {
   let validatedAddress = await validateAddress(address, provider);
   if (!validateAddress) {
-    alert('Invalid Ethereum address');
+    alert("Invalid Ethereum address");
     return;
   }
 
   // TODO!: should be from state, not localStorage, right?
-  const addressBundle = JSON.parse(localStorage.getItem('addressBundle')) || [];
+  const addressBundle = JSON.parse(localStorage.getItem("addressBundle")) || [];
   addressBundle.push(validatedAddress.address);
 
-  localStorage.setItem('addressBundle', JSON.stringify(addressBundle));
+  localStorage.setItem("addressBundle", JSON.stringify(addressBundle));
   setAddressBundle(addressBundle);
 
   if (validatedAddress.ensName) {
-    const newEnsMapping = {...ensMapping};
+    const newEnsMapping = { ...ensMapping };
     newEnsMapping[validatedAddress.address] = validatedAddress.ensName;
-    localStorage.setItem('ensMapping', JSON.stringify(newEnsMapping));
+    localStorage.setItem("ensMapping", JSON.stringify(newEnsMapping));
     setEnsMapping(newEnsMapping);
   }
 
   const url = new URL(window.location);
-  url.searchParams.set('addresses', addressBundle.join(','));
-  window.history.pushState({}, '', url);
+  url.searchParams.set("addresses", addressBundle.join(","));
+  window.history.pushState({}, "", url);
 };
 
 const EthereumAddressInput = ({ alchemy }) => {
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState("");
   const [addressBundle, setAddressBundle] = useState(() => {
-    const storedBundle = localStorage.getItem('addressBundle');
+    const storedBundle = localStorage.getItem("addressBundle");
     return storedBundle ? JSON.parse(storedBundle) : [];
   });
 
   const [ensMapping, setEnsMapping] = useState(() => {
-    const storedMapping = localStorage.getItem('ensMapping');
+    const storedMapping = localStorage.getItem("ensMapping");
     return storedMapping ? JSON.parse(storedMapping) : {};
   });
 
@@ -65,23 +70,33 @@ const EthereumAddressInput = ({ alchemy }) => {
   };
 
   const handleInputEnter = async (event) => {
-    if(event.key === 'Enter') {
-      processAddress(address, setAddressBundle, ensMapping, setEnsMapping,
-        await alchemy.config.getProvider());
-      setAddress('');
+    if (event.key === "Enter") {
+      processAddress(
+        address,
+        setAddressBundle,
+        ensMapping,
+        setEnsMapping,
+        await alchemy.config.getProvider()
+      );
+      setAddress("");
     }
   };
 
   const handleButtonClick = async () => {
-    processAddress(address, setAddressBundle, ensMapping, setEnsMapping,
-      await alchemy.config.getProvider());
-    setAddress('');
+    processAddress(
+      address,
+      setAddressBundle,
+      ensMapping,
+      setEnsMapping,
+      await alchemy.config.getProvider()
+    );
+    setAddress("");
   };
 
   useEffect(() => {
     const url = new URL(window.location);
-    url.searchParams.set('addresses', addressBundle.join(','));
-    window.history.pushState({}, '', url);
+    url.searchParams.set("addresses", addressBundle.join(","));
+    window.history.pushState({}, "", url);
   }, [addressBundle]);
 
   return (
@@ -94,9 +109,7 @@ const EthereumAddressInput = ({ alchemy }) => {
         onChange={handleInputChange}
         onKeyPress={handleInputEnter}
       />
-      <button onClick={handleButtonClick}>
-        +
-      </button>
+      <button onClick={handleButtonClick}>+</button>
     </div>
   );
 };
